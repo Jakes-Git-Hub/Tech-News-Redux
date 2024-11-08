1f1f4483cab1b38fe38e0fd0d79054a8cb40ad32
Jacob Matthews <jacmatthews7@gmail.com>
2024-11-08 10:06:37 +0000

    Fixed bug for articles and made tsx file for it

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,35 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
-import Search from "./Search";
-
-// Import Link and useSearchParams from React Router
-import { Link, useSearchParams } from 'react-router-dom';
-
-export default function Articles () {
-  const articles = useSelector(selectArticles);
-
-  // Grab URLSearchParams object from useSearchParams hook
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = searchParams.get('title');
-
-  const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
-
-  return (
-    <main>
-      <h1>Articles</h1>
-      <ul>
-        { filteredArticles.map(article => (
-          <li key={article.slug}>
-            <Link to={`${article.slug}`}>
-              {article.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Search />
-    </main>
-  )
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Articles;
+var react_1 = __importDefault(require("react"));
+var react_redux_1 = require("react-redux");
+var articlesSlice_1 = require("../features/articles/articlesSlice");
+var Search_1 = __importDefault(require("./Search"));
+var react_router_dom_1 = require("react-router-dom");
+function Articles() {
+    var articles = (0, react_redux_1.useSelector)(articlesSlice_1.selectArticles);
+    var _a = (0, react_router_dom_1.useSearchParams)(), searchParams = _a[0], setSearchParams = _a[1];
+    var title = searchParams.get('title');
+    var filteredArticles = title ? (0, articlesSlice_1.filterArticles)(title, articles) : Object.values(articles);
+    return (react_1.default.createElement("main", null,
+        react_1.default.createElement("h1", null, "Articles"),
+        react_1.default.createElement("ul", null, filteredArticles.map(function (article) { return (react_1.default.createElement("li", { key: article.slug },
+            react_1.default.createElement(react_router_dom_1.Link, { to: "".concat(article.slug) }, article.title))); })),
+        react_1.default.createElement(Search_1.default, null)));
 }
---
+++ src/components/Articles.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
+import Search from "./Search";
+import { Link, useSearchParams } from 'react-router-dom';
+
+interface Article {
+  slug: string;
+  title: string;
+}
+
+export default function Articles() {
+  const articles = useSelector(selectArticles) as Article[];
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const title = searchParams.get('title');
+
+  const filteredArticles: Article[] = title ? filterArticles(title, articles) : Object.values(articles);
+
+  return (
+    <main>
+      <h1>Articles</h1>
+      <ul>
+        {filteredArticles.map((article: Article) => (
+          <li key={article.slug}>
+            <Link to={`${article.slug}`}>
+              {article.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Search />
+    </main>
+  );
+}
