156746939f0394932f612efe84d3a565b06ac7a1
Jacob Matthews <jacmatthews7@gmail.com>
2024-11-14 10:09:31 +0000

    Added tsx file for Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,17 @@
-import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
-
-export default function Root() {
-    return (
-        <>
-            <Header/>
-            <main>
-                <Outlet/>
-            </main>
-            <Footer/>
-        </>
-    )
-}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var Header_1 = __importDefault(require("../components/Header"));
+var Footer_1 = __importDefault(require("../components/Footer"));
+var react_router_dom_1 = require("react-router-dom");
+var Root = function () {
+    return (react_1.default.createElement(react_1.default.Fragment, null,
+        react_1.default.createElement(Header_1.default, null),
+        react_1.default.createElement("main", null,
+            react_1.default.createElement(react_router_dom_1.Outlet, null)),
+        react_1.default.createElement(Footer_1.default, null)));
+};
+exports.default = Root;
