74fbe80d20931069a14ab61a7f6df20c7d9ec2e7
Jacob Matthews <jacmatthews7@gmail.com>
2024-11-14 10:17:15 +0000

    Made tsx file for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,20 @@
-import React from "react";
-import { useNavigate } from 'react-router-dom';
-
-export default function Footer() {
-
-  // get the navigate function
-  const navigate = useNavigate();
-
-  const goBack = () => {
-    // imperatively redirect back
-    navigate(-1);
-  }
-
-  const goForward = () => {
-    // imperatively redirect forward
-    navigate(1);
-  }
-
-  return (
-    <footer>
-      <button onClick={goBack}>Back</button>
-      <button onClick={goForward}>Forward</button>
-    </footer>
-  );
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Footer;
+var react_1 = __importDefault(require("react"));
+var react_router_dom_1 = require("react-router-dom");
+function Footer() {
+    var navigate = (0, react_router_dom_1.useNavigate)();
+    var goBack = function () {
+        navigate(-1);
+    };
+    var goForward = function () {
+        navigate(1);
+    };
+    return (react_1.default.createElement("footer", null,
+        react_1.default.createElement("button", { onClick: goBack }, "Back"),
+        react_1.default.createElement("button", { onClick: goForward }, "Forward")));
 }
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "../components/Footer";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 const Root: React.FC = () => {
