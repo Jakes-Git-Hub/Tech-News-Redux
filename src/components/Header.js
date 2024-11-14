b626493f9f8c482da506fc97ef64fafb6077589e
Jacob Matthews <jacmatthews7@gmail.com>
2024-11-14 10:14:36 +0000

    Made tsx file for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,24 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { selectCurrentUser, logOut } from "../features/session/sessionSlice"
-
-// Import the NavLink component.
-import { NavLink } from 'react-router-dom';
-
-export default function Header () {
-  const currentUser = useSelector(selectCurrentUser);
-  const dispatch = useDispatch();
-
-  const handleLogout = e => {
-    dispatch(logOut())
-  }
-
-  // Replace the 4 <a> tags with <NavLink> components
-  return (
-    <div className="header">
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/articles">Articles</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
-      {
-        currentUser.username
-          ? <>
-              <NavLink to="/profile">Profile</NavLink>
-              <button onClick={handleLogout} className="logout"> Log Out </button>
-            </>
-          : <NavLink to="/sign-up">Sign Up</NavLink>
-        }
-    </div>
-  )
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Header;
+var react_1 = __importDefault(require("react"));
+var react_redux_1 = require("react-redux");
+var sessionSlice_1 = require("../features/session/sessionSlice");
+var react_router_dom_1 = require("react-router-dom");
+function Header() {
+    var currentUser = (0, react_redux_1.useSelector)(sessionSlice_1.selectCurrentUser);
+    var dispatch = (0, react_redux_1.useDispatch)();
+    var handleLogout = function (e) {
+        dispatch((0, sessionSlice_1.logOut)());
+    };
+    return (react_1.default.createElement("div", { className: "header" },
+        react_1.default.createElement(react_router_dom_1.NavLink, { to: "/about" }, "About"),
+        react_1.default.createElement(react_router_dom_1.NavLink, { to: "/articles" }, "Articles"),
+        react_1.default.createElement(react_router_dom_1.NavLink, { to: "/categories" }, "Categories"),
+        currentUser.username ? (react_1.default.createElement(react_1.default.Fragment, null,
+            react_1.default.createElement(react_router_dom_1.NavLink, { to: "/profile" }, "Profile"),
+            react_1.default.createElement("button", { onClick: handleLogout, className: "logout" }, "Log Out"))) : (react_1.default.createElement(react_router_dom_1.NavLink, { to: "/sign-up" }, "Sign Up"))));
 }
